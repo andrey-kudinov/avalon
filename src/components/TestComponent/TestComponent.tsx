40e50591a2cf776237060d4ebcc8f42dac462e9f
Andrey Kudinov <2499931@gmail.com>
2022-06-14 22:39:59 +0400

    Add test component

---
+++ src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,3 @@
+import styles from '@/components/TestComponent/TestComponent.module.scss';
+
+export const TestComponent = () => <h3 className={styles.title}>Test Component!</h3>;
--- src/pages/second.tsx
+++ src/pages/second.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 
+import { TestComponent } from '@/components/TestComponent/TestComponent';
 import styles from '@/styles/Pages/Second.module.scss';
 
 const Second = () => (
@@ -7,7 +8,10 @@ const Second = () => (
     <Head>
       <title>Second</title>
     </Head>
+
     <h2 className={styles.title}>Second!</h2>
+
+    <TestComponent />
   </>
 );
 

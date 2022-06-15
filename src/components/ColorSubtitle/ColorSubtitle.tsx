8ea54702ce529100428ace4c40bb752cfccfec4f
Andrey Kudinov <2499931@gmail.com>
2022-06-16 00:47:55 +0400

    Add color subtitle

---
+++ src/components/ColorSubtitle/ColorSubtitle.tsx
@@ -0,0 +1,5 @@
+import styles from '@/components/ColorSubtitle/ColorSubtitle.module.scss';
+interface IProps {
+  title: string;
+}
+export const ColorSubtitle = ({ title }: IProps) => <h3 className={styles.title}>{title}</h3>;

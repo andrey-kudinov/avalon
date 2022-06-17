8fb12ea0e3ee5fc630da10f709fdd152b120df06
Andrey Kudinov <2499931@gmail.com>
2022-06-17 08:25:00 +0400

    Remove old components

--- src/components/SectionTitle/SectionTitle.tsx
+++
@@ -1,5 +0,0 @@
-import styles from '@/components/SectionTitle/SectionTitle.module.scss';
-interface IProps {
-  title: string;
-}
-export const SectionTitle = ({ title }: IProps) => <h2 className={styles.title}>{title}</h2>;
--- src/components/TestComponent/TestComponent.tsx
+++
@@ -1,3 +0,0 @@
-import styles from '@/components/TestComponent/TestComponent.module.scss';
-
-export const TestComponent = () => <h3 className={styles.title}>Test Component!</h3>;

51e6872b04319b4d3ae14128eb1e020425661a93
Andrey Kudinov <2499931@gmail.com>
2022-06-17 08:50:00 +0400

    Clean up a bit

--- src/components/ColorSubtitle/ColorSubtitle.tsx
+++ src/components/ColorSubtitle/ColorSubtitle.tsx
@@ -1,5 +1,7 @@
 import styles from '@/components/ColorSubtitle/ColorSubtitle.module.scss';
+
 interface IProps {
   title: string;
 }
+
 export const ColorSubtitle = ({ title }: IProps) => <h3 className={styles.title}>{title}</h3>;

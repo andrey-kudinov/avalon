7670b6b96c05d1b2a2060c930aa1157aa69cc8ab
Andrey Kudinov <2499931@gmail.com>
2022-06-25 21:25:39 +0400

    Update header

--- src/components/base/Header/Header.tsx
+++ src/components/base/Header/Header.tsx
@@ -1,44 +1,55 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
 import styles from '@/components/base/Header/Header.module.scss';
 import { SvgInline } from '@/components/SvgInline/SvgInline';
 
 import { Button } from '../Button/Button';
 
-export const Header = () => (
-  <header className={styles['site-header']}>
-    <div className={`${styles['header-wrapper']} container`}>
-      <Link href="/">
-        <a className={styles.brand}>
-          <SvgInline name="logo" width="249" height="46" />
-        </a>
-      </Link>
+export const Header = () => {
+  const [open, setOpen] = useState(false);
 
-      <nav className={styles.nav}>
-        <button className={styles.toggle} aria-expanded="false" type="button">
-          menu
-        </button>
+  return (
+    <header className={styles.header}>
+      <div className={`${styles['header-inner']} container`}>
+        <Link href="/">
+          <a className={styles.brand}>
+            <SvgInline name="logo" width="249" height="46" />
+          </a>
+        </Link>
 
-        <ul className={`${styles['nav-wrapper']} paragraph2`}>
-          <li className={styles.item}>
-            <Link href="/">Home</Link>
-          </li>
+        <nav className={styles.nav}>
+          <button
+            className={styles.toggle}
+            aria-expanded={open ? 'false' : 'true'}
+            aria-label={open ? 'close menu' : 'menu'}
+            type="button"
+            onClick={() => setOpen(!open)}
+          >
+            menu
+          </button>
 
-          <li className={styles.item}>
-            <Link href="/">Services</Link>
-          </li>
+          <ul className={`${styles['nav-wrapper']} paragraph2 ${open ? styles.active : ''}`}>
+            <li className={styles.item}>
+              <Link href="/">Home</Link>
+            </li>
 
-          <li className={styles.item}>
-            <Link href="/">Work</Link>
-          </li>
+            <li className={styles.item}>
+              <Link href="/">Services</Link>
+            </li>
 
-          <li className={styles.item}>
-            <Link href="/">About</Link>
-          </li>
+            <li className={styles.item}>
+              <Link href="/">Work</Link>
+            </li>
 
-          <Button title="Got a Project?" />
-        </ul>
-      </nav>
-    </div>
-  </header>
-);
+            <li className={styles.item}>
+              <Link href="/">About</Link>
+            </li>
+
+            <Button title="Got a Project?" />
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
